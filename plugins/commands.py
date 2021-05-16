feb5cab971d3b2f2ab1448ebb1263beafb89709a
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-05-16 13:14:51 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -8,4 +8,12 @@ async def login(c, m):
         cmd, api_key = m.text.split(' ')
         url = f"https://doodapi.com/api/account/info?key={api_key}"
         userdetails = requests.get(url).json()
-        await m.reply_text(userdetails)
+        if userdetails['status'] == 403:
+            text = "Send me the correct token"
+        if userdetails['status'] == 200:
+            text = "--**Your Details:**--\n\n"
+            text += f"**Email:** {userdetails['result']['email']}\n" if 'email' in userdetails['result'] else ""
+            text += f"**Balance:** {userdetails['result']['balance']}\n" if 'balance' in userdetails['result'] else ""   
+            text += f"**Storage left:** {userdetails['result']['storage_left']}\n" if 'storage_left' in userdetails['result'] else ""
+            text += f"**Premium Expiry:** {userdetails['result']['premim_expire']}\n" if 'premim_expire' in userdetails['result'] else "" 
+        await m.reply_text(text)
