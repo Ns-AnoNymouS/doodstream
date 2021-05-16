25927148aed5230ecdb561d7b691af69db873093
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-05-16 11:47:47 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -4,4 +4,5 @@ from pyrogram import Client, filters
 @Client_on_message(filters.command('token'))
 async def login(c, m):
     if len(m.command) == 2:
+        cmd, token = m.text.split(' ')
         url = f"https://doodapi.com/api/account/info?key={api_key}"
