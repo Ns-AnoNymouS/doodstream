d4b11f3839d603fa2c500c92abd0133681f54316
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-05-17 11:16:26 +0530

    Update open_file.py

--- doodstream/plugins/open_file.py
+++ doodstream/plugins/open_file.py
@@ -2,3 +2,9 @@ import requests
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
+
+@Client.on_callback_query(filters.regex('^file'))
+async def open_file(c, m):
+    cmd, file_code = m.data.split('+')
+    url = f"https://doodapi.com/api/file/info?key={your_api_key}&file_code={file_code}"
+    await m.message.edit()
