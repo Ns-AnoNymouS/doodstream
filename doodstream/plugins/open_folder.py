52933e616af02aa8d4028ee798320f23ab74d13b
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-05-17 15:10:09 +0530

    Update open_folder.py

--- doodstream/plugins/open_folder.py
+++ doodstream/plugins/open_folder.py
@@ -2,3 +2,6 @@ import requests
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
+
+@Client.on_callback_query(filters.regex('^folder'))
+async def folder(c, m):
