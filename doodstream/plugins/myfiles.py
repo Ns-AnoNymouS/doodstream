76c78d7db64de271112c2fa1f5e987e808e5b840
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-05-17 10:42:08 +0530

    Update myfiles.py

--- doodstream/plugins/myfiles.py
+++ doodstream/plugins/myfiles.py
@@ -47,6 +47,7 @@ async def nxt(c, m):
         for folder in folders:
             buttons.append([InlineKeyboardButton(f"📁 {folder['name']}", callback_data=f"folder+{folder['fld_id']}")])
         if len(folders) < 10:
+            print(fil)
             if fil < 0:
                 fil = 0
             files = data['result']['files'][fil: fil + 11]
