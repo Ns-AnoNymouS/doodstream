e0594bbb7754785442866d282be0c59ca1f33390
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-05-17 11:17:37 +0530

    Update open_file.py

--- doodstream/plugins/open_file.py
+++ doodstream/plugins/open_file.py
@@ -5,6 +5,9 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 @Client.on_callback_query(filters.regex('^file'))
 async def open_file(c, m):
+    api_key = await c.db.get_credential_status(m.from_user.id)
     cmd, file_code = m.data.split('+')
-    url = f"https://doodapi.com/api/file/info?key={your_api_key}&file_code={file_code}"
-    await m.message.edit()
+    url = f"https://doodapi.com/api/file/info?key={api_key}&file_code={file_code}"
+    data = requests.get(url).json()
+    print(data)
+    await m.message.edit(data)
