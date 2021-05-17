9bafb3963a6ee8adf3a4fee0490ca3590d78b4df
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-05-17 15:17:43 +0530

    Update open_folder.py

--- doodstream/plugins/open_folder.py
+++ doodstream/plugins/open_folder.py
@@ -19,7 +19,7 @@ async def folder(c, m):
         folders = data['result']['folders'][fld : fld + 11]
         buttons = []
         for folder in folders:
-            buttons.append([InlineKeyboardButton(f"📁 {folder['name']}", callback_data=f"folder+{folder['fld_id']}")])
+            buttons.append([InlineKeyboardButton(f"📁 {folder['name']}", callback_data=f"folder+{folder['fld_id']}+0+0")])
         if len(folders) < 10:
             if fil < 0:
                 fil = 0
