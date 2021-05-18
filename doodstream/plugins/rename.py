43031113e7c82e9a3e4b752d145b641ae5b16fb9
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-05-18 11:53:59 +0530

    Update rename.py

--- doodstream/plugins/rename.py
+++ doodstream/plugins/rename.py
@@ -6,6 +6,13 @@ from ..tools.progress_bar import humanbytes, TimeFormatter
 
 @Client.on_callback_query(filters.regex('^rename'))
 async def remame(c, m):
+    api_key = await c.db.get_credential_status(m.from_user.id)
+    try:
+        cmd, file_code, fld, fil = m.data.split('+')
+    except:
+        cmd, file_code = m.data.split('+')
+    url = f"https://doodapi.com/api/file/info?key={api_key}&file_code={file_code}"
+
     await Client.ask(
         self=c,
         chat_id=m.chat.id,
