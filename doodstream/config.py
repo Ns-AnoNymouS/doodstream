c5bcf8f509a52ced61e46ab7a4625fe49f1f48dd
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-05-18 19:36:30 +0530

    Update config.py

--- doodstream/config.py
+++ doodstream/config.py
@@ -4,6 +4,7 @@ class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     API_ID = int(os.environ.get("API_ID", 12345))
     API_HASH = os.environ.get("API_HASH")
+    ACTIVE_DOWNLOADS = []
     OWNER = os.environ.get("OWNER_ID", [1337144652])
     DATABASE_URL = os.environ.get("DATABASE_URL", "")
 
