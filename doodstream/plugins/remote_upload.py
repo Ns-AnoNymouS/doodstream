80e370c045b1e5bba22faf76a8b8102b2a7d5681
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-05-18 11:17:14 +0530

    Update remote_upload.py

--- doodstream/plugins/remote_upload.py
+++ doodstream/plugins/remote_upload.py
@@ -19,6 +19,7 @@ async def remote_upload(c, m):
 
 @Client.on_callback_query(filters.regex('^default$'))
 async def default(c, m):
+    await m.answer()
     upload_url = m.message.reply_to_message.text
     api_key = await c.db.get_credential_status(m.from_user.id)
     url = f"https://doodapi.com/api/upload/url?key={api_key}&url={upload_url}"
@@ -48,4 +49,38 @@ async def default(c, m):
         if json_data['result'][0]['status'] == 'error':
             await m.message.edit('The link you provided is not valid or not in supporting Sites so please try with different link')
     except:
-        await m.message.edit('sucess')
+        file_code = data['result']['filecode']
+        url = f"https://doodapi.com/api/file/info?key={api_key}&file_code={file_code}"
+        files_url = f"https://doodapi.com/api/file/list?key={api_key}"
+        data_file = requests.get(files_url).json()
+        files = data_file['result']['files'] #.find_one({'filecode':file_code})
+        for file in files:
+            if file['file_code'] == file_code:
+                file_data = file
+                break
+        data = requests.get(url).json()
+
+        if data['status'] == 200:
+            text = f"**📁 Title:** {data['result'][0]['title']}\n\n"
+            text += f"**⏰ Duration:** {TimeFormatter(int(data['result'][0]['length']) * 1000)}\n\n"
+            text += f"**📊 Size:** {humanbytes(int(data['result'][0]['size']))}\n\n"
+            text += f"**👁 Views:** {data['result'][0]['views']}\n\n"
+            text += f"**📆 Uploaded on:** {data['result'][0]['uploaded']}"
+            buttons = [[
+                InlineKeyboardButton("Rename ✏", callback_data=f"rename+{data['result'][0]['filecode']}"),
+                InlineKeyboardButton("Download 📥", url=f"{file_data['download_url']}"),
+                ],[
+                InlineKeyboardButton("Watch Online 👀", url=f"https://dood.so{data['result'][0]['protected_embed']}")
+            ]]
+            return await m.message.edit(text, reply_markup=InlineKeyboardMarkup(buttons))
+
+        elif data['status'] == 451:
+            text = "Your video was disabled due to DMCA"
+
+        elif data['status'] == 403:
+            text = "Your TOKEN was expired. So please logout and login again"
+ 
+        else:
+            text = "File Not Found 🤪"
+
+        await m.message.edit(text)
