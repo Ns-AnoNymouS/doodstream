ab6884c68277e25d5372a015128ab42ab1649a51
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-05-18 11:45:49 +0530

    Update rename.py

--- doodstream/plugins/rename.py
+++ doodstream/plugins/rename.py
@@ -1,1 +1,8 @@
+import requests
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from ..tools.progress_bar import humanbytes, TimeFormatter
 
+
+@Client.on_callback_query(filters.regex('^rename'))
+aaync def remame(c, m):
