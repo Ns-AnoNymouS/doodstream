91621f64b9baf5060829c523c67cbe41e0c595d4
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-05-18 08:37:30 +0530

    Update remote_upload.py

--- doodstream/plugins/remote_upload.py
+++ doodstream/plugins/remote_upload.py
@@ -6,4 +6,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 @Client.on_message(filters.regex('.*http.*'))
 async def remote_upload(c, m):
     upload_url = m.text
-    url = f"https://doodapi.com/api/upload/url?key={your_api_key}&url={upload_url}"
+    api_key = await c.db.get_credential_status(m.from_user.id)
+    url = f"https://doodapi.com/api/upload/url?key={api_key}&url={upload_url}"
+    data = requests.post(url)
+    print(data.text)
