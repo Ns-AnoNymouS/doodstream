9464cb7d064df700d73af2ec580d9a3c5fe87902
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-05-18 14:52:20 +0530

    Update remote_actions.py

--- doodstream/plugins/remote_actions.py
+++ doodstream/plugins/remote_actions.py
@@ -5,7 +5,12 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 @Client.on_message(filters.command('remote_actions))
 async def actions(c, m):
+    api_key = await c.db.get_credential_status(m.from_user.id)
     url = f"https://doodapi.com/api/urlupload/slots?key={api_key}"
-    buttons = [[
+    list_uploads = f"https://doodapi.com/api/urlupload/list?key={api_key}"
+    data = requests.get(url).json()
+    remote_list = requests.get(list_uploads).json()
+    print(data, remote_list)
+    """buttons = [[
     ]]
-    await m.reply_text()
+    await m.reply_text()"""
