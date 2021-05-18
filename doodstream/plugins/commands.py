9f15715a568eb83ce7a8e27715084fa8f7e04639
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-05-18 22:16:37 +0530

    Update commands.py

--- doodstream/plugins/commands.py
+++ doodstream/plugins/commands.py
@@ -35,4 +35,7 @@ async def token(c, m):
 
 @Client.on_callback_query(filters.private & filters.incoming)
 async def token_check(c, m):
-    
+    api_key = await c.db.get_credential_status(m.from_user.id)
+    if not api_key:
+        return await m.reply_text("You didn't Authorize me yet")
+    await m.continue_propagation()
