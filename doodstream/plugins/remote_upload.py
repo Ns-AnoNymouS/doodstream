30fd3b9f8d9f2a57e7553ab985237bbe33fc3308
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-05-18 09:01:46 +0530

    Update remote_upload.py

--- doodstream/plugins/remote_upload.py
+++ doodstream/plugins/remote_upload.py
@@ -1,11 +1,22 @@
 import requests
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from ..name import 
+from ..name import isdownloadable_link
+
 
 @Client.on_message(filters.regex('.*http.*'))
 async def remote_upload(c, m):
     upload_url = m.text
+    status, name = isdownloadable_link(upload_url)
+    buttons = [[
+        InlineKeyboardButton('Default', callback_data='default'),
+        InlineKeyboardButton('Rename', callback_data='rename')
+    ]]
+    await m.reply_text(f'File Name: {name}', reply_markup=InlineKeyboardMarkup(buttons))
+
+
+@Client.on_callback_query(filters.regex('^default$'))
+    upload_url = m.reply_to_message.text
     api_key = await c.db.get_credential_status(m.from_user.id)
     url = f"https://doodapi.com/api/upload/url?key={api_key}&url={upload_url}"
     data = requests.get(url)
