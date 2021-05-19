a2bb88331df9b001c158df92f349e5827ba647ee
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-05-19 15:01:54 +0530

    Update status.py

--- doodstream/plugins/status.py
+++ doodstream/plugins/status.py
@@ -9,9 +9,12 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 async def status(c, m):
     api_key = await c.db.get_credential_status(m.from_user.id)
     url = f"https://doodapi.com/api/account/info?key={api_key}"
-    userdetails = requests.get(url).json()
+    loop = asyncio.get_event_loop()
+    with concurrent.futures.ThreadPoolExecutor() as pool:
+        userdetails = await loop.run_in_executor(pool, requests.get, url)
+    userdetails = userdetails.json()
     if userdetails['status'] == 403:
-        text = "Send me the correct token"
+        text = "Your Token expired"
     elif userdetails['status'] == 200:
         await c.db.update_credential_status(m.from_user.id,  api_key)
         text = "--**Your Details:**--\n\n"
@@ -20,6 +23,6 @@ async def status(c, m):
         text += f"**📈 Storage left:** {userdetails['result']['storage_left']}\n" if 'storage_left' in userdetails['result'] else ""
         text += f"**💠 Premium Expiry:** {userdetails['result']['premim_expire']}\n" if 'premim_expire' in userdetails['result'] else "" 
     else:
-        text = "Something Went wrong"
+        text = f"Error: {userdetails['msg']}"
 
     await m.reply_text(text)
