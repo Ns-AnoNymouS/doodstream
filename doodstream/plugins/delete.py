3ddd469191a4466e55b02a6be1669feba5bee806
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-07-09 19:11:27 +0530

    Update delete.py

--- doodstream/plugins/delete.py
+++ doodstream/plugins/delete.py
@@ -5,14 +5,15 @@ from pyrogram import Client, filters
 @Client.on_callback_query(filters.regex(''))
 async def delete(c, m):
     data = m.data.split('+')
+    folderId = 0
     if len(data) == 2:
         cmd, fileId = data
     else:
         cmd, fileId, folderId = data
 
     postData = {
-        'fld_id: 0,
-        'file_id': 28386261,
+        'fld_id: int(folderId),
+        'file_id': int(fileId),
         'op': 'videos_json',
         'del_selected': 'Delete selected',
         'token': a44f6bd5ce74c6e5f263727a47f02638
