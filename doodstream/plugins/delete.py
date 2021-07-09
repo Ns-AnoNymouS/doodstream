151ba9ff1c26b7e989118a5782b5773f83533967
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2021-07-09 19:03:24 +0530

    Update delete.py

--- doodstream/plugins/delete.py
+++ doodstream/plugins/delete.py
@@ -3,4 +3,10 @@ from pyrogram import Client, filters
 
 @Client.on_callback_query(filters.regex(''))
 async def delete(c, m):
+    data = m.data.split('+')
+    if len(data) == 2:
+        cmd, fileId = data
+    else:
+        cmd, fileId, folderId = data
+
     
