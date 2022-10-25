65702294ccbc2f11c51ff46e48545ad611c0fed6
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2022-10-25 11:30:53 +0530

    update

--- doodstream/tools/text.py
+++ doodstream/tools/text.py
@@ -47,21 +47,3 @@ I can upload tg files to your doodstream account too. Check help button for more
 10. Don't argue with admins."""
 
       LOGIN = """Only for admins for receiving feedbacks"""
-
-
-      ABOUT = """**MY DETAILS:**
-
-```🤖My Name:``` [Feedback Ns Bot](https://t.me/Feedback_Nsbot)
-    
-```📝 Language:``` [Python 3](https://www.python.org/)
-
-```🧰 Framework:``` [Pyrogram](https://github.com/pyrogram/pyrogram)
-
-```👨🏻‍💻 Developer:``` [𝐀𝐧𝐨𝐧𝐲𝐦𝐨𝐮𝐬](https://t.me/Ns_AnoNymouS)
-               
-```📢 Channel:``` [NS BOT UPDATES](https://t.me/Ns_bot_updates)
-
-```👥 Group:``` [Ns BOT SUPPORT](https://t.me/Ns_Bot_supporters)
-
-Inspired By @stemlime_bot
-"""
