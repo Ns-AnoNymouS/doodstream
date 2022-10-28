888e345808396c65b4495271f4782d0836cf0786
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2022-10-28 11:42:30 +0530

    ok

--- doodstream/plugins/login.py
+++ doodstream/plugins/login.py
@@ -4,4 +4,9 @@ from pyrogram import Client, filters
 @Client.on_message(filters.command('login') & filters.private & filters.incoming)
 async def login(client, message):
     dood = DoodStream()
-    await dood.login('Ns_Anonymous', 'Naveen9329@')
+    username = await client.ask(message.from_user.id, 'send your username')
+    password = await client.ask(message.from_user.id, 'send your password')
+    sts = await dood.login(username.text, password.text)
+    if sts == 'otp_sent':
+        otp = await client.ask(message.from_user.id, 'send the opt sent to your email.')
+        sts = await dood.login(username.text, password.text, otp.text)
